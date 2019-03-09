2e41929944cdca29fc650a6ea5d048438c03cbc3
KhurshidYakubov <khurshid.yakubov@gmail.com>
2019-03-09 18:16:55 +0500

    2 change

--- webpage/ex1.php
+++ webpage/ex1.php
@@ -17,9 +17,6 @@ if(preg_match($pattern, $text)){
 }
 }
 
-
-
-
 ?>
 
 
@@ -35,7 +32,7 @@ if(preg_match($pattern, $text)){
 		<dl>
 
             <dt>Text</dt>
-			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
+			<dd><textarea name="text"  cols="30" rows="10"><?= $text ?></textarea></dd>
 			<dt>String ( Example: /sometext/ )</dt>
 			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
 
@@ -43,7 +40,10 @@ if(preg_match($pattern, $text)){
 			<dt>&nbsp;</dt>
 			<dd><input type="submit" value="Check"></dd>
 		</dl>
-          <?php echo $contain ;?>
+        <?php echo $contain ;?> <br>  <br>
+		After removing whitespaces of the text: <?php echo preg_replace('/\s+/', '', $text)."\n";?> <br> <br><!--for removing whitespace-->
+		After removing non-numeric characters of the text: <?php echo preg_replace("/[^0-9,.]/", "", $text)."\n"; ?> <br> <br><!--for removing non-numeric characters-->
+            <!--  <?php echo preg_replace('/\n/', '' , $text) ?> <br> <br> -->
 	</form>
 </body>
 </html>
